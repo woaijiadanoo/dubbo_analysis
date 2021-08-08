10eb200beea899f7a768bcb8d4c62ec400161f26
jiangzheng <jz292679@sina.com>
2021-08-08 19:58:31 +0800

    first commit

---
+++ dubbo-config/dubbo-config-spring/src/main/java/org/apache/dubbo/config/spring/schema/DubboNamespaceHandler.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.config.spring.schema;
+
+import org.apache.dubbo.common.Version;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ConsumerConfig;
+import org.apache.dubbo.config.MetadataReportConfig;
+import org.apache.dubbo.config.MetricsConfig;
+import org.apache.dubbo.config.ModuleConfig;
+import org.apache.dubbo.config.MonitorConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.ProviderConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.SslConfig;
+import org.apache.dubbo.config.spring.ConfigCenterBean;
+import org.apache.dubbo.config.spring.ReferenceBean;
+import org.apache.dubbo.config.spring.ServiceBean;
+import org.apache.dubbo.config.spring.beans.factory.config.ConfigurableSourceBeanMetadataElement;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.w3c.dom.Element;
+
+import static org.apache.dubbo.config.spring.util.DubboBeanUtils.registerCommonBeans;
+
+/**
+ * DubboNamespaceHandler
+ *
+ * @export
+ */
+public class DubboNamespaceHandler extends NamespaceHandlerSupport implements ConfigurableSourceBeanMetadataElement {
+
+    static {
+        Version.checkDuplicate(DubboNamespaceHandler.class);
+    }
+
+    /**
+     *   这里是DubboNamespaceHandler的核心方法，启动流程所需内容都在这里
+     */
+    @Override
+    public void init() {
+        // 从这里开始是一系列初始化，在Dubbo的配置文件中配置的几乎所有内容都是在这里进行读取和初始化的
+        registerBeanDefinitionParser("application", new DubboBeanDefinitionParser(ApplicationConfig.class));
+        registerBeanDefinitionParser("module", new DubboBeanDefinitionParser(ModuleConfig.class));
+        registerBeanDefinitionParser("registry", new DubboBeanDefinitionParser(RegistryConfig.class));
+        registerBeanDefinitionParser("config-center", new DubboBeanDefinitionParser(ConfigCenterBean.class));
+        registerBeanDefinitionParser("metadata-report", new DubboBeanDefinitionParser(MetadataReportConfig.class));
+        registerBeanDefinitionParser("monitor", new DubboBeanDefinitionParser(MonitorConfig.class));
+        registerBeanDefinitionParser("metrics", new DubboBeanDefinitionParser(MetricsConfig.class));
+        registerBeanDefinitionParser("ssl", new DubboBeanDefinitionParser(SslConfig.class));
+        registerBeanDefinitionParser("provider", new DubboBeanDefinitionParser(ProviderConfig.class));
+        registerBeanDefinitionParser("consumer", new DubboBeanDefinitionParser(ConsumerConfig.class));
+        registerBeanDefinitionParser("protocol", new DubboBeanDefinitionParser(ProtocolConfig.class));
+
+        // 当所有内容都初始化完成以后，就要开始进行启动的准备工作了
+        // ServiceBean主要是针对provider端的，<dubbo:service... 开头的内容都是由这里进行处理的
+        registerBeanDefinitionParser("service", new DubboBeanDefinitionParser(ServiceBean.class));
+        // ReferenceBean主要是针对consumer端的，<dubbo:reference... 开头的内容都是由这里进行处理的
+        registerBeanDefinitionParser("reference", new DubboBeanDefinitionParser(ReferenceBean.class));
+        // 这里是进行全注解的处理的
+        registerBeanDefinitionParser("annotation", new AnnotationBeanDefinitionParser());
+    }
+
+    /**
+     * Override {@link NamespaceHandlerSupport#parse(Element, ParserContext)} method
+     *
+     * @param element       {@link Element}
+     * @param parserContext {@link ParserContext}
+     * @return
+     * @since 2.7.5
+     */
+    @Override
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        BeanDefinitionRegistry registry = parserContext.getRegistry();
+        registerAnnotationConfigProcessors(registry);
+        /**
+         * @since 2.7.8
+         * issue : https://github.com/apache/dubbo/issues/6275
+         */
+        registerCommonBeans(registry);
+        BeanDefinition beanDefinition = super.parse(element, parserContext);
+        setSource(beanDefinition);
+        return beanDefinition;
+    }
+
+    /**
+     * Register the processors for the Spring Annotation-Driven features
+     *
+     * @param registry {@link BeanDefinitionRegistry}
+     * @see AnnotationConfigUtils
+     * @since 2.7.5
+     */
+    private void registerAnnotationConfigProcessors(BeanDefinitionRegistry registry) {
+        AnnotationConfigUtils.registerAnnotationConfigProcessors(registry);
+    }
+}
